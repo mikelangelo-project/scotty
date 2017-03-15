b0a820af8845a2cdd89d293245b1a8249ff2c035
Maik Srba <msrba@gwdg.de>
2017-03-15 17:15:57 +0000

    add scotty config parser to utils

--- scotty/utils.py
+++ scotty/utils.py
@@ -1,5 +1,7 @@
 import argparse
 import logging
+import os
+import ConfigParser
 
 import scotty
 
@@ -25,3 +27,45 @@ class Args(object):
 
     def exec_command(self):
         scotty.cli.COMMANDS[self._args.command].main(self)
+
+class Config(object):
+    class __Config:
+        def __init__(self):
+            script_dir = os.path.dirname(os.path.realpath(__file__))
+            self._base_dir = self._find_base_dir()
+            self._path =  os.path.normpath(self._base_dir + '/scotty.conf')
+            self._load()
+
+        def _find_base_dir(self):
+            if os.path.isfile('/etc/scotty/scotty.conf'):
+                return '/etc/scotty/'
+            else:
+                script_dir = os.path.dirname(os.path.realpath(__file__))
+                base_dir = os.path.normpath(script_dir + '/../etc/')
+                config_path = os.path.normpath(base_dir + '/scotty.conf')
+                if os.path.isfile(config_path):
+                    return base_dir
+                else:
+                    raise Exception('Found no configuration for scotty (/etc/scotty/scotty.conf or ./python-scotty/etc/scotty.conf)')
+
+        def _load(self):
+            self._config = ConfigParser.ConfigParser()
+            self._config.read(self._path)
+
+        def _abspath(self, path):
+            if not os.path.isabs(path):
+                path = os.path.normpath(self._base_dir + '/'+ path)
+            return path
+
+        def get(self, section, option, abspath=False):
+            value = self._config.get(section, option)
+            if abspath: value = self._abspath(value)
+            return value 
+
+    instance = None
+    def __init__(self):
+        if not Config.instance:
+            Config.instance = Config.__Config()
+
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
