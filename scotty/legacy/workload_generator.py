d5abd8df540b53ff8b49209ee189761385b94d29
Maik Srba <msrba@gwdg.de>
2017-03-20 08:49:33 +0000

    first step workload-generator legacy

--- scotty/legacy/workload_generator.py
+++ scotty/legacy/workload_generator.py
@@ -1,8 +1,26 @@
 import logging
 
-class WorkloadGenerator(object):
+import scotty.utils as utils
+
+LOG = logging.getLogger(__name__)
+
+class Workflow(object):
     def __init__(self, args):
         self._args = args
 
-    def main(self):
-        logging.info('Workload generator main')
+    def run(self):
+        self._checkout()
+        self._workload_run()
+ 
+    def _checkout(self):
+        # TODO load environment variables
+        # TODO checkout workload change
+        # TODO copy workload.yaml from workload test directory
+        LOG.info('Checkout workload')
+    
+    def _workload_run(self):
+        # TODO run workload  
+        LOG.info('Run workload') 
+        if self._args.getargs().not_run:
+            LOG.info('    found option --not-run (-n): skip run workload')
+            return
