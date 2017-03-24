90132198423f118a6df33fca90ca0b15eb423b2d
Maik Srba <msrba@gwdg.de>
2017-03-24 09:31:30 +0000

    Format code wich flake8 Named decorator parameters classes in scotty.cmd.base.CommandRegistry

--- scotty/cmd/base.py
+++ scotty/cmd/base.py
@@ -1,44 +1,47 @@
 class CommandRegistry(object):
     registry = {}
- 
-    @classmethod
-    def addparser(cls, parser):
-        module_ = parser.__module__
-        key     = cls.getcommandkey(module_)
-        cls.registry[key] = cls.registry.get(key, {})
-        cls.registry[key]['parser'] = parser 
-        return parser
 
     @classmethod
-    def addbuilder(cls, builder):
-        module_ = builder.__module__
-        key     = cls.getcommandkey(module_)
+    def addparser(cls, parser_class):
+        module_ = parser_class.__module__
+        key = cls.getcommandkey(module_)
         cls.registry[key] = cls.registry.get(key, {})
-        cls.registry[key]['builder'] = builder
-        return builder
+        cls.registry[key]['parser'] = parser_class
+        return parser_class
+
+    @classmethod
+    def addbuilder(cls, builder_class):
+        module_ = builder_class.__module__
+        key = cls.getcommandkey(module_)
+        cls.registry[key] = cls.registry.get(key, {})
+        cls.registry[key]['builder'] = builder_class
+        return builder_class
 
     @classmethod
     def getcommandkey(cls, module_):
         key = module_.rsplit('.', 1)[-1]
         return key
- 
+
     @classmethod
     def getbuilder(cls, command):
-        builder = cls.registry[command]['builder']
-        return builder()
+        builder_class = cls.registry[command]['builder']
+        return builder_class()
 
     @classmethod
     def getparser(cls, command):
-        parser = cls.registry[command]['parser']
-        return parser()
+        parser_class = cls.registry[command]['parser']
+        return parser_class()
+
 
 class CommandParser(object):
     def add_arguments(self, parser):
         raise NotImplementedError(
             'You must implement CommandParser.add_arguments(self, parser)')
-        
+
+
 class CommandBuilder(object):
     command_class = None
+
     def buildCommand(self, options):
         if not self.command_class:
             raise NotImplementedError(
