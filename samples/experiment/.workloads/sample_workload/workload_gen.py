c46326cc3e6859658b8babc117e0aa0b00a0b2f1
Maik Srba <msrba@gwdg.de>
2017-04-24 17:15:49 +0000

    add sample for experiment

---
+++ samples/experiment/.workloads/sample_workload/workload_gen.py
@@ -0,0 +1,15 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def run(context):
+    name = context.v1.workload_config.name
+    print 'Hey there,'
+    print 'my name is {name} '.format(name=name),
+    print 'and I\'m running a dummy workload with my config:'
+    print '{context}'.format(context=context.v1.workload_config._dict)
+    print 'params'
+    print '{params}'.format(params=context.v1.workload_config.params)
+    raise Exception('TestException')
