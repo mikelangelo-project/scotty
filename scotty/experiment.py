f1d6e118f0ff9b6d2516f24d705c9ed93bafe122
Maik Srba <msrba@gwdg.de>
2017-04-24 17:51:24 +0000

    Add run workflow for experiment and checkout parameters

--- scotty/experiment.py
+++ scotty/experiment.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import git
 
@@ -7,10 +8,47 @@ import scotty.utils as utils
 logger = logging.getLogger(__name__)
 
 
+class Workspace(object):
+    def __init__(self, path, git_=None):
+        self.path = path
+        if git_ is None:
+            self._git = git.cmd.Git
+        else:
+            self._git = git_
+
+
 class Workflow(object):
     def __init__(self, options):
         self._options = options
         self.workspace = None
 
     def run(self):
-        logger.info('Run experiment.Workflow')
+        self._prepare()
+        self._checkout()
+        self._load()
+        self._run()
+
+    def _prepare(self):
+        path = os.path.abspath(self._options.workspace)
+        self.workspace = self.workspace or Workspace(path)
+
+    def _checkout(self):
+        if self._options.skip_checkout:
+            return
+        try:
+            project = self._options.project or os.environ['ZUUL_PROJECT']
+            zuul_url = self._options.zuul_url or os.environ['ZUUL_URL']
+            zuul_ref = self._options.zuul_ref or os.environ['ZUUL_REF']
+        except KeyError as e:
+            message = 'Missing Zuul settings ({})'.format(e)
+            logger.error(message)
+            raise CheckoutException(message)
+                
+    def _load(self):
+        pass
+
+    def _run(self):
+        pass
+
+class CheckoutException(Exception):
+    pass
