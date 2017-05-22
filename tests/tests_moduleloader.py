92b27ef2076e2dd3e37f0d4e1a9e1bf3a113128b
Maik Srba <msrba@gwdg.de>
2017-05-22 15:03:12 +0000

    fix naming in module loader test

--- tests/tests_moduleloader.py
+++ tests/tests_moduleloader.py
@@ -5,7 +5,7 @@ import imp
 from scotty.core.moduleloader import ModuleLoader
 
 
-class WorkloadModuleLoaderTest(unittest.TestCase):
+class ModuleLoaderTest(unittest.TestCase):
     workload_path = 'samples/components/workload/workload_gen.py'
 
     def setUp(self):
@@ -17,13 +17,13 @@ class WorkloadModuleLoaderTest(unittest.TestCase):
         mod.__dict__.update(**args)
         return mod
 
-    def test_load_workload_no_name(self):
+    def test_load_module_no_name(self):
         workload_ = self._module_loader.load_by_path(self.workload_path)
         self.assertTrue('run' in dir(workload_))
         module_ = self.make_module('scotty.workload_gen.anonymous_workload')
         self.assertEqual(workload_, module_)
 
-    def test_load_workload_with_name(self):
+    def test_load_module_with_name(self):
         workload_ = self._module_loader.load_by_path(self.workload_path, 'test_workload')
         self.assertTrue('run' in dir(workload_))
         module_ = self.make_module('scotty.workload_gen.test_workload')
