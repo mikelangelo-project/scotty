e6a46d0a42f02b6ba532fd7be4bac136fa622d93
Peter Chronz <peter.chronz@gmail.com>
2017-06-14 10:24:07 +0200

    refactor CheckoutManager for legibility

--- scotty/core/checkout.py
+++ scotty/core/checkout.py
@@ -17,21 +17,30 @@ class CheckoutManager(object):
         logging.getLogger('git.repo.base').setLevel(logging.WARNING)
 
     def checkout(self, git_url, workspace, git_ref=None):
-        if not self.is_git_dir(workspace.path):
-            repo = git.Repo.clone_from(git_url, workspace.path)
-        else:
-            repo = git.Repo(workspace.path)
-        repo.git.remote('update')
-        repo.git.reset('--hard')
-        repo.git.clean('-x', '-f', '-d', '-q')
-        if git_ref:
-            repo.git.fetch(git_url, git_ref)
-            repo.git.checkout('FETCH_HEAD')
-            repo.git.reset('--hard', 'FETCH_HEAD')
+        repo = self._get_repo(git_url, workspace.path)
+        self._sync_repo(repo)
+        self._checkout_ref(repo, git_ref)
         self._init_submodules(workspace, repo)
 
     def is_git_dir(self, path):
         return os.path.isdir('{path}/.git'.format(path=path))
+        
+    def _get_repo(self, git_url, workspace_path):
+        if not self.is_git_dir(workspace.path):
+            repo = git.Repo.clone_from(git_url, workspace.path)
+        else:
+            repo = git.Repo(workspace.path)
+            
+    def _sync_repo(self, repo):
+        repo.git.remote('update')
+        repo.git.reset('--hard')
+        repo.git.clean('-x', '-f', '-d', '-q')
+        
+    def _checkout_ref(self, repo, git_ref):
+        if git_ref:
+            repo.git.fetch(git_url, git_ref)
+            repo.git.checkout('FETCH_HEAD')
+            repo.git.reset('--hard', 'FETCH_HEAD')
 
     def _init_submodules(self, workspace, repo):
         if os.path.isfile('{path}/.gitmodules'.format(path=workspace.path)):
