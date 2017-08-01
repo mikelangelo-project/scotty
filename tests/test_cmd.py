5817b7f241cd1c88c0450b4c289567c902c5a253
Peter Chronz <peter.chronz@gmail.com>
2017-08-01 14:48:13 +0200

    Refactor test_cmd and add tests for the commands: experiment & workload.

---
+++ tests/test_cmd.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import argparse
+
+from scotty.cmd.base import CommandRegistry
+
+
+class CommandTest(object):
+    def _test_parser(self):
+        parser = argparse.ArgumentParser()
+        subparser = parser.add_subparsers(dest='command')
+        for key in CommandRegistry.registry:
+            subparser.add_parser(key)
+        args = self.args[0:1]
+        command_options = parser.parse_args(args)
+        command_builder = CommandRegistry.getbuilder(command_options.command)
+        command_parser = CommandRegistry.getparser(command_options.command)
+        command_class = CommandRegistry.getcommand_class(command_options.command)
+        args = self.args[1:]
+        resource_parser = argparse.ArgumentParser()
+        command_parser.add_arguments(resource_parser)
+        resource_options = resource_parser.parse_args(args)
+        self.assertEquals(resource_options.action, self.args[1])
+        cmd = command_builder.buildCommand(resource_options, command_class)
+        self.assertIsInstance(cmd, self.command_class)
+        cmd.execute()
--- tests/test_cmd_resource.py
+++ tests/test_cmd_resource.py
@@ -1,30 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-import argparse
 
-from scotty.cmd.base import CommandRegistry
 import scotty.cmd.resource
 
+from tests.test_cmd import CommandTest
 
-# TODO refactor
-# TODO mock cmd.execute()
-class ResourceCommandTest(unittest.TestCase):
-    def test_parser(self):
-        parser = argparse.ArgumentParser()
-        subparser = parser.add_subparsers(dest='command')
-        for key in CommandRegistry.registry:
-            subparser.add_parser(key)
-        args = ['resource']
-        command_options = parser.parse_args(args)
-        command_builder = CommandRegistry.getbuilder(command_options.command)
-        command_parser = CommandRegistry.getparser(command_options.command)
-        command_class = CommandRegistry.getcommand_class(command_options.command)
-        args = ['init']
-        resource_parser = argparse.ArgumentParser()
-        command_parser.add_arguments(resource_parser)
-        resource_options = resource_parser.parse_args(args)
-        self.assertEquals(resource_options.action, 'init')
-        cmd = command_builder.buildCommand(resource_options, command_class)
-        self.assertIsInstance(cmd, scotty.cmd.resource.Command)
-        cmd.execute()
+
+class ResourceCommandTest(unittest.TestCase, CommandTest):
+    def test_init(self):
+        self.command_class = scotty.cmd.resource.Command
+        self.args = ['resource', 'init']
+        self._test_parser()
