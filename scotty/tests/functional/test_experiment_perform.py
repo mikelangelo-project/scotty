cecda644ff835913004aa8992dab302fcfa7bc54
msrba <msrba@gwdg.de>
2017-10-13 17:47:54 +0200

    more tests in experiment perform

--- scotty/tests/functional/test_experiment_perform.py
+++ scotty/tests/functional/test_experiment_perform.py
@@ -15,23 +15,28 @@ class PerformExperimentTest(unittest.TestCase):
         if os.path.isdir(self.experiment_tmp_path):
             shutil.rmtree(self.experiment_tmp_path)
         shutil.copytree(self.experiment_samples_path, self.experiment_tmp_path)
+        self.experiment_scotty_path = os.path.join(self.experiment_tmp_path, '.scotty/')
+        self.setUpWorkloadModulePath()
+        self.setUpResourceModulePath()
+
+    def setUpWorkloadModulePath(self):
+        self.workload_module_path = os.path.join(
+            self.experiment_scotty_path,
+            'components/workloads/demo_workload/workload_gen.py')
+
+    def setUpResourceModulePath(self):
+        self.resource_module_path = os.path.join(
+            self.experiment_scotty_path,
+            'components/resources/demo_resource/resource_gen.py')
 
     def tearDown(self):
-        pass
-        #shutil.rmtree(self.experiment_tmp_path)
+        shutil.rmtree(self.experiment_tmp_path)
 
     def test_perform_experiment(self):
         self.run_cmd()
-        experiment_scotty_path = os.path.join(self.experiment_tmp_path, '.scotty/')
-        workload_module_path = os.path.join(
-            experiment_scotty_path, 
-            'components/workloads/demo_workload/workload_gen.py')
-        resource_module_path = os.path.join(
-            experiment_scotty_path,
-            'components/resources/demo_resource/resource_gen.py')
-        self.assertTrue(os.path.isdir(experiment_scotty_path), 'Missing .scotty directory')
-        self.assertTrue(os.path.exists(workload_module_path), 'Missing module for demo_workload')
-        self.assertTrue(os.path.exists(resource_module_path), 'Missing module for demo_resource')
+        self.assertTrue(os.path.isdir(self.experiment_scotty_path), 'Missing .scotty directory')
+        self.assertTrue(os.path.exists(self.workload_module_path), 'Missing module for demo_workload')
+        self.assertTrue(os.path.exists(self.resource_module_path), 'Missing module for demo_resource')
         #check resource result
         #check workload result 
 
