b0f9e7cd818f3c0c0c400c22be8b459cf36f98de
msrba <msrba@gwdg.de>
2017-10-13 15:29:50 +0200

    prepare samples for testing, add more gitignores, copy samples in tmp

--- scotty/tests/functional/test_experiment_perform.py
+++ scotty/tests/functional/test_experiment_perform.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 import contextlib
+import shutil
 
 from scotty import cli 
 
@@ -10,17 +11,27 @@ class PerformExperimentTest(unittest.TestCase):
         'experiment', 'perform'
     ]
 
-    experiment_path = 'samples/components/experiment/'
-        
+    experiment_samples_path = 'samples/components/experiment/single_workload'
+    experiment_tmp_path = os.path.join('tmp', experiment_samples_path)
+
+    def setUp(self):
+        shutil.copytree(self.experiment_samples_path, self.experiment_tmp_path)
+        pass #copy samples to tmp
+
+    def tearDown(self):
+        pass #delete samples from tmp
+
+    def test_perform_experiment(self):
+        with self.cwd(self.experiment_tmp_path):
+            cli_ = cli.Cli()
+            cli_.parse_command(self.args[0:1])
+        #schauen ob .scotty verzeichniss geschrieben wurde
+        #schauen ob resource ergebniss richtig geschrieben
+        #schauen ob workload ergebnisse richtig geschrieben hat 
+
     @contextlib.contextmanager
     def cwd(self, path):
         prev_cwd = os.getcwd()
         os.chdir(path)
         yield
         os.chdir(prev_cwd)
-
-    def test_perform_experiment(self):
-        with self.cwd(self.experiment_path):
-            cli_ = cli.Cli()
-            cli_.parse_command(self.args[0:1])
-        
