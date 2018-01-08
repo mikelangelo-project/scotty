e7b13aa1a1f09bf8d51620f2f7e2d723f53cddb2
Maik Srba <msrba@gwdg.de>
2018-01-08 13:53:14 +0100

    implement resultstore utils

--- scotty/utils.py
+++ scotty/utils.py
@@ -7,6 +7,15 @@ from scotty.core.exceptions import ScottyException
 logger = logging.getLogger(__name__)
 
 
+class BaseUtils(object):
+    def __init__(self, context):
+        self.context = context
+        self.__experiment = context.v1._ContextV1__experiment
+
+    @property
+    def experiment_uuid(self):
+        return self.__experiment.uuid
+
 class ExperimentHelper(object):
     def __init__(self, context):
         # TODO validate context - is from scotty and not a fake from customer component
@@ -41,16 +50,15 @@ class ExperimentHelper(object):
         return self.__experiment.uuid
 
 
-class WorkloadUtils(object):
+class WorkloadUtils(BaseUtils):
     def __init__(self, context):
-        self.context = context
+        super(WorkloadUtils, self).__init__(context)
         try:
             self.current_workload = context.v1.workload
         except KeyError as e:
             logger.error('WorkloadUtils can only used in workload context')
             raise
-        self.__experiment = context.v1._ContextV1__experiment
-        experiment_workspace = self.__experiment.workspace
+        experiment_workspace = self._BaseUtils__experiment.workspace
         self.component_data_path = experiment_workspace.get_component_data_path(self.current_workload, True)
 
     def _real_path(self, file_):
@@ -61,3 +69,30 @@ class WorkloadUtils(object):
     def open_file(self, file_, mode):
         real_path = self._real_path(file_)
         return open(real_path, mode)
+
+class ResultstoreUtils(BaseUtils):
+    def __init__(self, context):
+        super(ResultstoreUtils, self).__init__(context)
+        try:
+            self.current_resultstore = context.v1.resultstore
+        except KeyError as e:
+            logger.error('ResultStoreUtils can only used in resultstore context')
+            raise
+
+    @property
+    def workloads(self):
+        workloads = self._BaseUtils__experiment.components['workload']
+        return workloads
+
+    @property
+    def remote_base_dir(self):
+        return "scotty"
+
+    @property
+    def remote_experiment_dir(self):
+        remote_experiment_dir = os.path.join(self.remote_base_dir, str(self.experiment_uuid))
+        return remote_experiment_dir
+
+    def local_result_dir(self, workload_name):
+        local_result_dir = os.path.join('.scotty/data/workload', workload_name)
+        return local_result_dir
